de8e8eba80ff62b2ef5f32d9fbfeb842d52a7839
unknown <andre.lb865854@gmail.com>
2026-02-24 13:17:30 -0300

    pages and components

--- travel-website/src/App.js
+++ travel-website/src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
       <Router>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<NavBar />} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Router>
     </>
